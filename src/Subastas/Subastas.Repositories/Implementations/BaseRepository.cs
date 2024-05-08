45c955e53c709ff1fad7ee8aa139f4c3bb77d052
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-05-07 23:42:13 -0600

    :sparkles: Add Repositories Proyect

---
+++ src/Subastas/Subastas.Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Subastas.DAL;
+using System.Collections.Generic;
+
+namespace Subastas.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly SubastasContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(SubastasContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
