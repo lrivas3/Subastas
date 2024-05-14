48c9581125a4db2d15fac823cf474186ad8a1a47
memo124 <48780097+memo124@users.noreply.github.com>
2024-05-13 22:17:43 -0600

    Se agrego nueva rama y un diseño nuevo

---
+++ src/Subastas/Subastas/Controllers/SubastaController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Subastas.DAL;
+
+namespace Subastas.Controllers
+{
+    public class SubastaController(SubastasContext _contex) : Controller
+    {
+        // GET: SubastaController
+        public ActionResult Index()
+        {
+            var listSubasta = _contex.Subastas.ToList();
+            return View(listSubasta);
+        }
+
+        // GET: SubastaController/Details/5
+        public ActionResult Details(int id)
+        {
+            var subasta = _contex.Subastas.Include(x=>x.Pujas).ThenInclude(x=>x.IdUsuarioNavigation).Where(x=>x.IdSubasta == id).FirstOrDefault();
+            
+            return View(subasta);
+        }
+
+        // GET: SubastaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SubastaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SubastaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SubastaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SubastaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SubastaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- src/Subastas/Subastas/Program.cs
+++ src/Subastas/Subastas/Program.cs
@@ -33,7 +33,7 @@ namespace Subastas
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Login}/{action=Index}/{id?}");
 
             app.Run();
         }
