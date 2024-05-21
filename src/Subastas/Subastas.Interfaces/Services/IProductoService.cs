f5308caf46e912be98cb38cdf9baf1b919d1dba3
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-05-20 21:49:41 -0600

    :construction_worker: implement producto interfaces, repo and service

---
+++ src/Subastas/Subastas.Interfaces/Services/IProductoService.cs
@@ -0,0 +1,15 @@
+﻿using Subastas.Domain;
+
+namespace Subastas.Interfaces
+{
+    public interface IProductoService
+    {
+        Task<IEnumerable<Producto>> GetAllAsync();
+        Task<Producto> CreateAsync(Producto newProducto);
+        Task<Producto> CreateIfNotExistsAsync(Producto newProducto);
+        Task<bool> ExistsByIdAsync(int idProducto);
+        Task<bool> ExistsByNameAsync(string ProductoName);
+        Task<Producto> GetByIdAsync(int idProducto);
+        Task<Producto> GetByNameAsync(string ProductoName);
+    }
+}
---
+++ src/Subastas/Subastas.Services/ProductoService.cs
@@ -0,0 +1,57 @@
+﻿using Subastas.Domain;
+using Subastas.Interfaces;
+
+namespace Subastas.Services
+{
+    public class ProductoService(IProductoRepository productoRepository) : IProductoService
+    {
+        public async Task<IEnumerable<Producto>> GetAllAsync()
+        {
+            return await productoRepository.GetAllAsync();
+        }
+
+        public async Task<Producto> CreateAsync(Producto newProducto)
+        {
+            if (newProducto == null)
+                return null;
+
+            await productoRepository.AddAsync(newProducto);
+
+            return newProducto;
+        }
+
+        public async Task<Producto> CreateIfNotExistsAsync(Producto newProducto)
+        {
+            if (newProducto == null)
+                return null;
+
+            if (newProducto.IdProducto > 0 && await ExistsByIdAsync(newProducto.IdProducto))
+                return await GetByIdAsync(newProducto.IdProducto);
+
+            if (!string.IsNullOrEmpty(newProducto.NombreProducto) && await ExistsByNameAsync(newProducto.NombreProducto))
+                return await GetByNameAsync(newProducto.NombreProducto);
+
+            return await CreateAsync(newProducto);
+        }
+
+        public async Task<bool> ExistsByIdAsync(int idProducto)
+        {
+            return await productoRepository.ExistsByPredicate(p => p.IdProducto.Equals(idProducto));
+        }
+
+        public async Task<bool> ExistsByNameAsync(string ProductoName)
+        {
+            return await productoRepository.ExistsByPredicate(p => p.NombreProducto == ProductoName);
+        }
+
+        public async Task<Producto> GetByIdAsync(int idProducto)
+        {
+            return await productoRepository.GetByIdAsync(idProducto);
+        }
+
+        public async Task<Producto> GetByNameAsync(string ProductoName)
+        {
+            return await productoRepository.GetByPredicate(p => p.NombreProducto == ProductoName);
+        }
+    }
+}
