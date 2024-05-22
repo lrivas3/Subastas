996dca638cabad60ac9ab122aba303f551c0167f
Leonardo Rivas <84354110+lrivas3@users.noreply.github.com>
2024-05-21 23:58:33 -0600

    :sparkles: Save Producto image to s3

--- src/Subastas/Subastas.Dto/Producto/ProductoCreateRequest.cs
+++ src/Subastas/Subastas.Dto/Producto/ProductoCreateRequest.cs
@@ -1,6 +1,13 @@
-﻿namespace Subastas.Dto.Producto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Subastas.Dto.Producto;
 
 public class ProductoCreateRequest
 {
-    
+    public string NombreProducto { get; set; } = null!;
+
+    [MaxLength(500)]
+    public string DescripcionProducto { get; set; } = null!;
+
+    public bool EstaActivo { get; set; }
 }
--- src/Subastas/Subastas/Controllers/ProductoController.cs
+++ src/Subastas/Subastas/Controllers/ProductoController.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Subastas.Domain;
+using Subastas.Dto.Producto;
 using Subastas.Interfaces;
-using Subastas.Services;
+using Subastas.Dto.S3;
+using Subastas.Interfaces.Services;
 
 namespace Subastas.Controllers
 {
     [Authorize(AuthenticationSchemes = "Bearer")]
-    public class ProductoController(IProductoService productoService) : Controller
+    public class ProductoController : Controller
     {
+        private readonly IProductoService _productoService;
+        private readonly IS3StorageService _s3StorageService;
+        private readonly IConfiguration _config;
+        private readonly string _bucketName;
+        private readonly AwsCredentials _awsCredentials;
+
+        public ProductoController(IProductoService productoService, IS3StorageService s3StorageService,
+            IConfiguration config)
+        {
+            _productoService = productoService;
+            _s3StorageService = s3StorageService;
+            _config = config;
+            _bucketName = _config["AwsConfiguration:SubastasBucket"];
+            _awsCredentials = new AwsCredentials
+            {
+                AwsKey = _config["AwsConfiguration:AwsAccessKey"],
+                AwsSecretKey = _config["AwsConfiguration:AwsSecretKey"]
+            };
+        }
+
         public async Task<IActionResult> Index()
         {
-            var listaSubasta = await productoService.GetAllAsync();
+            var listaSubasta = await _productoService.GetAllAsync();
             return View(listaSubasta);
         }
 
@@ -22,15 +44,51 @@ namespace Subastas.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("NombreProducto,DescripcionProducto,ImagenProducto,EstaActivo")] Producto producto)
+        public async Task<IActionResult> Create(
+            [Bind("NombreProducto,DescripcionProducto,EstaActivo")] ProductoCreateRequest productoRequest,
+            IFormFile imagen)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                producto.EstaSubastado = false;
-                await productoService.CreateAsync(producto);
-                return RedirectToAction(nameof(Index));
+                return View(productoRequest);
             }
-            return View(producto);
+
+            // Crear una instancia de Producto desde ProductoCreateRequest
+            var producto = new Producto
+            {
+                NombreProducto = productoRequest.NombreProducto,
+                DescripcionProducto = productoRequest.DescripcionProducto,
+                EstaActivo = productoRequest.EstaActivo,
+                EstaSubastado = false // Valor predeterminado, ya que no se subasta inmediatamente al crear
+            };
+
+            if (imagen != null && imagen.Length > 0)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    await imagen.CopyToAsync(stream);
+
+                    var s3Object = new S3Object
+                    {
+                        InputStream = stream,
+                        Name = Guid.NewGuid().ToString() + Path.GetExtension(imagen.FileName),
+                        BucketName = _bucketName
+                    };
+
+                    var response = await _s3StorageService.UploadObjectAsync(s3Object, _awsCredentials);
+
+                    if (response.StatusCode != 200)
+                    {
+                        ModelState.AddModelError(string.Empty, "Error uploading image to S3: " + response.Message);
+                        return View(productoRequest);
+                    }
+
+                    producto.ImagenProducto = s3Object.Name;
+                }
+            }
+
+            await _productoService.CreateAsync(producto);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
