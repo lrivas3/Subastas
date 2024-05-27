b58373eeafa609fea72de6f1e3bd8ef324fb16d2
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-05-26 18:36:27 -0600

    :hammer: expand decimals 8,2 to 16,2

---
+++ src/Subastas/Subastas.Database/Migrations/20240527003545_Decimal 8,2 to 16,2.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Subastas.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Decimal82to162 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_transaccion",
+                schema: "subastas",
+                table: "transacciones",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_inicial",
+                schema: "subastas",
+                table: "subastas",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_puja",
+                schema: "subastas",
+                table: "pujas",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "saldo",
+                schema: "subastas",
+                table: "cuenta",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_transaccion",
+                schema: "subastas",
+                table: "transacciones",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_inicial",
+                schema: "subastas",
+                table: "subastas",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_puja",
+                schema: "subastas",
+                table: "pujas",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "saldo",
+                schema: "subastas",
+                table: "cuenta",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+        }
+    }
+}
