9d077e4a52f437a94e6230ab290ee4aa6d999853
memo124 <48780097+memo124@users.noreply.github.com>
2024-05-30 07:03:35 -0600

    Se agrego las validaciones al modelado de producto y usuario

--- src/Subastas/Subastas.Domain/Cuenta.cs
+++ src/Subastas/Subastas.Domain/Cuenta.cs
@@ -1,11 +1,15 @@
-﻿namespace Subastas.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Subastas.Domain;
 
 public partial class Cuenta
 {
     public int IdCuenta { get; set; }
-
+    [Display(Name = "Saldo de la cuenta")]
+    [Range(0, double.MaxValue, ErrorMessage = "El saldo debe ser un número positivo")]
+    [Required(ErrorMessage = "El saldo es obligatorio")]
     public decimal Saldo { get; set; }
-
+    [Display(Name = "Estado de la cuenta")]
     public bool EstaActivo { get; set; } = true;
 
     public int IdUsuario { get; set; }
--- src/Subastas/Subastas.Domain/Usuario.cs
+++ src/Subastas/Subastas.Domain/Usuario.cs
@@ -1,17 +1,23 @@
-﻿namespace Subastas.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Subastas.Domain;
 
 public partial class Usuario
 {
     public int IdUsuario { get; set; }
-
+    [Display(Name = "Nombre del usuario")]
+    [Required(ErrorMessage = "El nombre del usuario es obligatorio")]
     public string NombreUsuario { get; set; } = null!;
-
+    [Display(Name = "Apellido del usuario")]
+    [Required(ErrorMessage = "El Apellido del usuario es obligatorio")]
     public string ApellidoUsuario { get; set; } = null!;
-
+    [Display(Name = "Correo del usuario")]
+    [Required(ErrorMessage = "El correo del usuario es obligatorio")]
     public string CorreoUsuario { get; set; } = null!;
-
+    [Display(Name = "Contraseña del usuario")]
+    [Required(ErrorMessage = "La contraseña del usuario es obligatorio")]
     public string Password { get; set; } = null!;
-
+    [Display(Name = "Estado del usuario")]
     public bool EstaActivo { get; set; } = true;
 
     public virtual Cuenta? Cuentum { get; set; }
