81653806d14d6729b72426baf0a01d8c1745d28b
AlexisUFG <ia.alexismartinez@ufg.edu.sv>
2024-11-17 21:09:03 -0600

    :hammer: add migration decimal up to 26

--- src/Subastas/Subastas.Database/Migrations/20241118030848_update decimal count.Designer.cs
+++ src/Subastas/Subastas.Database/Migrations/20241118030848_update decimal count.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Subastas.Database;
 
@@ -11,9 +12,11 @@ using Subastas.Database;
 namespace Subastas.Database.Migrations
 {
     [DbContext(typeof(SubastasContext))]
-    partial class SubastasContextModelSnapshot : ModelSnapshot
+    [Migration("20241118030848_update decimal count")]
+    partial class updatedecimalcount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Subastas/Subastas.Database/Migrations/20241118030848_update decimal count.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Subastas.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedecimalcount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                schema: "subastas",
+                table: "usuarios",
+                type: "varchar(150)",
+                unicode: false,
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldUnicode: false,
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_transaccion",
+                schema: "subastas",
+                table: "transacciones",
+                type: "decimal(26,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_inicial",
+                schema: "subastas",
+                table: "subastas",
+                type: "decimal(26,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_puja",
+                schema: "subastas",
+                table: "pujas",
+                type: "decimal(26,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "saldo",
+                schema: "subastas",
+                table: "cuenta",
+                type: "decimal(26,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                schema: "subastas",
+                table: "usuarios",
+                type: "varchar(150)",
+                unicode: false,
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldUnicode: false,
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_transaccion",
+                schema: "subastas",
+                table: "transacciones",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(26,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_inicial",
+                schema: "subastas",
+                table: "subastas",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(26,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "monto_puja",
+                schema: "subastas",
+                table: "pujas",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(26,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "saldo",
+                schema: "subastas",
+                table: "cuenta",
+                type: "decimal(16,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(26,2)");
+        }
+    }
+}
